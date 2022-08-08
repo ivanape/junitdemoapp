a5cd15766b195f13bc4976bf4050ffc26298b8fe
Iván Álvarez <ivanape@ext.inditex.com>
2022-08-08 15:19:22 +0200

    DbCalculator throws exception

--- src/main/java/com/programwar/DbCalculator.java
+++ src/main/java/com/programwar/DbCalculator.java
@@ -8,25 +8,25 @@ public class DbCalculator {
         this.dbRepository = dbRepository;
     }
 
-    public int sum(int n1, int n2) {
+    public int sum(int n1, int n2) throws Exception {
         int result = n1 + n2;
         dbRepository.save(result);
         return result;
     }
 
-    public int subs(int n1, int n2) {
+    public int subs(int n1, int n2) throws Exception {
         int result = n1 - n2;
         dbRepository.save(result);
         return result;
     }
 
-    public int mult(int n1, int n2) {
+    public int mult(int n1, int n2) throws Exception {
         int result = n1 * n2;
         dbRepository.save(result);
         return result;
     }
 
-    public int div(int n1, int n2) {
+    public int div(int n1, int n2) throws Exception {
         int result = n1 * n2;
         dbRepository.save(result);
         return result;
--- src/main/java/com/programwar/DbRepository.java
+++ src/main/java/com/programwar/DbRepository.java
@@ -1,9 +1,12 @@
 package com.programwar;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class DbRepository {
 
-    public void save(int value) {
+    public void save(int value) throws Exception {
         System.out.println("Saving value: " + value);
+        throw new NotImplementedException();
     }
 
 }
